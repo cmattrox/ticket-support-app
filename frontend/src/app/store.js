8c163a0557752f4c1d650c7063571335d5a2ff90
Charlie Welch <welch.charlie16@gmail.com>
2022-03-10 15:45:08 -0600

    Finish logic and styling for create new ticket

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import ticketSlice from '../features/tickets/ticketSlice';
 
 export const store = configureStore({
-	reducer: { auth: authReducer },
+	reducer: { auth: authReducer, ticket: ticketSlice },
 });
