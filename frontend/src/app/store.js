8d45a7fa7c6ed2795bee8ac35732783b36d198e6
Charlie Welch <welch.charlie16@gmail.com>
2022-03-14 20:25:34 -0500

    Creating state for getting users tickets

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,5 +3,5 @@ import authReducer from '../features/auth/authSlice';
 import ticketSlice from '../features/tickets/ticketSlice';
 
 export const store = configureStore({
-	reducer: { auth: authReducer, ticket: ticketSlice },
+	reducer: { auth: authReducer, tickets: ticketSlice },
 });
